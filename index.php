a959d01be1f8abedc948b83adcdffbda497ef2d3
yanira22 <>
2019-03-06 21:49:35 -0300

    primeros archivos

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body> 
+        Texto Sin Programar
+        <br>
+        <?php
+        /*para definir una variable usamos el signo $*/
+        $saludo="Mundo Variable"; /*VARIABLE*/
+        echo "Hola Mundo";
+        echo "Hola ".$saludo; /*LLAMAMOS VARIABLE*/
+        ?>
+        
+        <br>
+        Variables Goblajes S_SERVER
+        <br>
+        <pre>
+        <?php
+        var_dump($_SERVER);/*Imprime contenido del directorio*/ 
+        ?>
+        </pre>
+        
+    </body>
+</html>
