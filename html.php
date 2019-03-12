ab165d1b713c4964bf69d0d1bd681ce59697c195
cetecom <cetecom@LCV10813>
2019-03-11 21:46:48 -0300

    estilos Final ejercicio 2

--- html.php
+++ html.php
@@ -1,4 +1,6 @@
-<div></div>
-<div>Mensaje <?php echo $texto;?></div>
-<div></div>
+<div class="parrafos1">Primer Texto</div>
+<div style="font-size: 20px;color: green;background: yellow;">Mensaje <?php echo $texto;?></div>
+<div class="parrafos">Segundo Texto</div>
+<p id="parrafo1">Parrafo1</p>
+<p id="parrafo2">Parrafo2</p>
 
